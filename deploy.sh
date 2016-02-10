78969d99a88ec0dc605c63a305d637f47851eb1b
Hoonmin Kim <hoonmin.kim@navercorp.com>
2016-02-10 13:06:04 +0900

    removed public directory

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:dorothy/dorothy.github.io.git gh-pages
+git push origin project
+git subtree push --prefix=public git@github.com:dorothy/dorothy.github.io.git master
